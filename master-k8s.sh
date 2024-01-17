a3ef2a96a4ab0b3712dc7d5cc6a0993549aeda43
Filippo Spinella <fili.spin2003@gmail.com>
2024-01-17 12:35:01 +0100

    Update master-k8s.sh

--- master-k8s.sh
+++ master-k8s.sh
@@ -2,9 +2,6 @@
 #
 # Setup for Control Plane (Master) servers
 
-
-
-PUBLIC_IP_ACCESS="true"
 NODENAME=$(hostname -s)
 
 # Pull required images
@@ -23,10 +20,6 @@ sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 # Install Claico Network Plugin Network 
 
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
-
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml -O
-
-kubectl create -f custom-resources.yaml
 
 wget https://github.com/derailed/k9s/releases/download/v0.31.5/k9s_linux_arm.deb
 
