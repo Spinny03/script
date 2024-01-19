5865a6be8be3d4b3fc34b8dcf101c75220d2278e
Filippo Spinella <fili.spin2003@gmail.com>
2024-01-19 14:46:41 +0100

    Update master-k8s.sh

--- master-k8s.sh
+++ master-k8s.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
-#
-# Setup for Control Plane (Master) servers
-
-NODENAME=$(hostname -s)
-
-# Pull required images
 
 sudo kubeadm config images pull
-
 sudo kubeadm init --ignore-preflight-errors Swap
 
-
-# Configure kubeconfig
 
 mkdir -p "$HOME"/.kube
 sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
 
-# Install Claico Network Plugin Network 
-
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
 
 wget https://github.com/derailed/k9s/releases/download/v0.31.5/k9s_linux_amd64.deb
 
